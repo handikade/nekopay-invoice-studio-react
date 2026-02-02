956c3a7c6865d13fdb93df89e2b97a726c3c175d
handikade <dika.csui@gmail.com>
2026-02-02 21:03:00 +0700

    feat: default value for testing

--- src/pages/invoice/InvoicePage.tsx
+++ src/pages/invoice/InvoicePage.tsx
@@ -1,7 +1,3 @@
-import {
-  invoiceSchema,
-  type Invoice,
-} from "@handikade/nekopay-invoice-studio-schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Box,
@@ -24,6 +20,7 @@ import InvoicePreviewer from "./InvoicePreviewer";
 import InvoiceWizardHeader from "./components/InvoiceWizardHeader";
 import InvoiceTemplateDefault from "./components/templates/InvoiceTemplateDefault";
 import { downloadPdf } from "./pdf";
+import { invoiceSchema, type Invoice } from "./schema";
 
 const glassCardSx = {
   flexGrow: 1,
@@ -41,10 +38,61 @@ const InvoicePreviewPanel = () => {
   return <InvoiceTemplateDefault invoice={invoice} />;
 };
 
+// const issueDate = new Date().toISOString;
+// const dueDate = new Date(Date.now() + 7 * 24 * 60 * 60 * 1_000).toISOString;
+
 const InvoicePage = () => {
   const [activeStep, setActiveStep] = useState(0);
   const methods = useForm<Invoice>({
     resolver: zodResolver(invoiceSchema),
+    defaultValues: {
+      from: {
+        address:
+          "Jl. Johar Baru Utara VI No. 19, RT 08 / RW 05, Johar Baru, Jakarta Pusat, 10560",
+        email: "dika.csui@gmail.com",
+        name: "Dika Handika",
+        phone: "08988945464",
+      },
+      to: {
+        address:
+          "Jl. Komplek Bulog Blok C No. 28, RT 06 / RW 06, Pulo Gadung, Jakarta TImur, 10322",
+        email: "asep.show@gmail.com",
+        name: "Asep Show",
+        phone: "08988945464",
+      },
+      currency: "IDR",
+      issueDate: new Date(),
+      dueDate: new Date(),
+      id: "123412341234",
+      invoiceNumber: "INV/2026/01/001",
+      items: [
+        {
+          description: "Susu Segar",
+          discount: 10,
+          discountType: "percentage",
+          id: "item-001",
+          price: 10000,
+          quantity: 10,
+        },
+        {
+          description: "Indomie Goreng",
+          discount: 5000,
+          discountType: "fixed",
+          id: "item-002",
+          price: 3500,
+          quantity: 40,
+        },
+      ],
+      footer: {
+        notes:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        terms:
+          "Aenean egestas suscipit feugiat. Ut luctus nibh id cursus dapibus. Nam suscipit risus at dui.",
+        signatureTextHeader: "Hormat Kami",
+        signatureURL: undefined,
+        signatureTextFooter: "Finance",
+      },
+    },
   });
   const previewRef = useRef<HTMLDivElement>(null);
 
